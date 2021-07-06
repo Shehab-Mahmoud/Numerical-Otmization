8ab5289bb8f5bf6b52e2a2b2020e271f2a21fdf4
Shehab Mahmoud <42918713+shehab2@users.noreply.github.com>
2021-07-06 09:29:57 +0200

    Update Stocastic-Batch-Minibatch-GD.py

--- Stocastic-Batch-Minibatch-GD.py
+++ Stocastic-Batch-Minibatch-GD.py
@@ -1,7 +1,7 @@
 # import libraries
-from os import truncate
 import numpy as np
 import matplotlib.pyplot as plt
+np.random.seed(2040)
 
 # training data
 x_points =np.array( [[1,1,2,3,4,5,6,7,8,9,10,11]]).T
@@ -17,9 +17,17 @@ def intialize_params(X):
 def hyp(theta0,theta1,X):
     return theta0 + theta1*X
 
-# function to compute Stocastic cost
-def compute_cost_stocastic(theta0,theta1,X,Y):
-    return (1/2)*((hyp(theta0,theta1,X)-Y))**2
+# function to caculate batch cost
+def compute_cost(theta0,theta1,X,Y,m):
+    return (1/(2*m))*np.sum((hyp(theta0,theta1,X)-Y)**2)
+
+
+# calculate Batch Grads
+def calculate_grads_batch(X,Y,theta0,theta1,m):
+    dtheta0 = (1/m)*np.sum(hyp(theta0,theta1,X)-Y)
+    dtheta1 = float((1/m)*X.T.dot(hyp(theta0,theta1,X)-Y))
+    
+    return dtheta0,dtheta1
 
 # calculate Stocastic grads
 def calculate_grads_stocastic(X,Y,theta0,theta1):
@@ -35,25 +43,63 @@ def update_params(theta0,theta1,learning_rate,dtheta0,dtheta1):
     
     return theta0, theta1
 
-def fit_model(X,Y,learning_rate = 0.001,tolerance = 1e-5,plot_result=True,epochs = 1000):
+def fit_model(X,Y,learning_rate = 0.001,tolerance = 1e-5,n_batches = 2,plot_result=False,kind ="SGD"):
     theta0, theta1,m = intialize_params(X)
 
-    iters = 0
-    while(True):
-        indexes = np.random.randint(0, len(X)) # random sample
-        
-        Xs = np.take(X, indexes)
-        ys = np.take(Y, indexes)
+    # Batch Gradient descent
+    if(kind=="batch"):
+        iters = 0
+        while(True):
 
-        cost_old = compute_cost_stocastic(theta0,theta1,Xs,ys)
-        dtheta0 , dtheta1 = calculate_grads_stocastic(Xs,ys,theta0,theta1)
-        theta0, theta1 = update_params(theta0,theta1,learning_rate,dtheta0,dtheta1)
-        cost_new = compute_cost_stocastic(theta0,theta1,Xs,ys)   
+            cost_old = compute_cost(theta0,theta1,X,Y,m)
+            dtheta0 ,dtheta1 = calculate_grads_batch(X,Y,theta0,theta1,m)
+            theta0,theta1 = update_params(theta0,theta1,learning_rate,dtheta0,dtheta1)
+            cost_new = compute_cost(theta0,theta1,X,Y,m)
+            iters += 1 
+            if((cost_old-cost_new)<=tolerance):
+                break
         
-        iters +=1
-        if((cost_old-cost_new)<=tolerance):
-            break
+    # mini-batch Gradient descent
+    if(kind == "mini-batch"):
+
+        iters = 0
+        batch_size = int(len(X)/n_batches)
+
+        while(True):
+            batch_index = np.random.randint(0,len(X-1),batch_size)
+
+            x_batch = np.take(X,batch_index)
+            y_batch = np.take(Y,batch_index)
+
+            cost_old = compute_cost(theta0,theta1,x_batch,y_batch,m)
+            dtheta0 ,dtheta1 = calculate_grads_batch(x_batch,y_batch,theta0,theta1,m)
+            theta0,theta1 = update_params(theta0,theta1,learning_rate,dtheta0,dtheta1)
+            cost_new = compute_cost(theta0,theta1,x_batch,y_batch,m)
+
+            iters += 1 
+            if((cost_old-cost_new)<=tolerance):
+                break
+        
+
+    # Stocastic Gradient descent
+    if(kind == "SGD"):
+        iters = 0
+        while(True):
+            indexes = np.random.randint(0, len(X)) # random sample
+            
+            Xs = np.take(X, indexes)
+            ys = np.take(Y, indexes)
+
+            cost_old = compute_cost(theta0,theta1,Xs,ys,1)
+            dtheta0 , dtheta1 = calculate_grads_stocastic(Xs,ys,theta0,theta1)
+            theta0, theta1 = update_params(theta0,theta1,learning_rate,dtheta0,dtheta1)
+            cost_new = compute_cost(theta0,theta1,Xs,ys,1)   
+            
+            iters +=1
+            if((cost_old-cost_new)<=tolerance):
+                break
     # plot fitted line w.r.t data
+
     if(plot_result):
         plt.scatter(x_points,y_points)
 
@@ -64,13 +110,19 @@ def fit_model(X,Y,learning_rate = 0.001,tolerance = 1e-5,plot_result=True,epochs
         plt.grid()
         plt.legend(["Linear regression fit","Data"])
 
-    print(iters)
-    return theta0,theta1
+    
+    return theta0,theta1,iters
 
 
 
-theta0,theta1 = fit_model(x_points,y_points)
-print(theta0,theta1)
+theta0,theta1,iters = fit_model(x_points,y_points,kind="batch",plot_result=False)
+print("\n\nbatch : \ntheta0 = "+str(theta0)+"\ntheta1 = "+str(theta1)+"\nNumbers of itterations to converse : "+str(iters)+"\n\n")
+
+theta0,theta1,iters = fit_model(x_points,y_points,kind="SGD",plot_result=True)
+print("SGD : \ntheta0 = "+str(theta0)+"\ntheta1 = "+str(theta1)+"\nNumbers of itterations to converse : "+str(iters)+"\n\n")
+
+theta0,theta1,iters = fit_model(x_points,y_points,kind="mini-batch",plot_result=False)
+print("mini-batch :\ntheta0 = "+str(theta0)+"\ntheta1 = "+str(theta1)+"\nNumbers of itterations to converse : "+str(iters))
 
 
 
